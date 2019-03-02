97c2b1484131af5d4c0a60ab8e0742a55e95381b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-02 12:12:21 -1000

    yeet

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-content class="grey lighten-4">
+      <Header/>
+    </v-content>
+    <v-footer class="grey lighten-2" height="auto">
+    <Footer/>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  },
+  data () {
+    return {
+      //
+    }  
+  }
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout justify-center row wrap>
+      <v-btn v-for="link in links" :key="link" color="grey" flat round>
+        {{ link }}
+      </v-btn>
+      <v-btn color="grey" flat round>
+        <v-icon>code</v-icon>
+      </v-btn>
+
+      <v-flex grey lighten-3 py-3 text-xs-center grey--text xs12 >
+        <strong>Waipahu High School</strong>
+      </v-flex>
+    </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+          'About',
+          'Contact Us',
+      ]
+
+    })
+  }
+</script>
